8a2e7e7bf8a0bae325d71d54c36557bca73f123b
Leonardo Figueiredo <leonardo.figueiredo@ipflagship.com.br>
2023-12-28 15:48:57 -0300

    chore(dizzle): add drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
 import { Elysia } from "elysia";
+import { db } from "./db";
+import { sql } from "drizzle-orm";
+import * as schema from "./schema";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => {
+    const query = sql`select "hello world" as text`;
+    const result = db.get<{ text: string }>(query);
+    return result;
+  })
+  .get("/seed", async () => {
+    await db.insert(schema.movies).values([
+      {
+        title: "The Matrix",
+        releaseYear: 1999,
+      },
+      {
+        title: "The Matrix Reloaded",
+        releaseYear: 2003,
+      },
+      {
+        title: "The Matrix Revolutions",
+        releaseYear: 2003,
+      },
+    ]);
+
+    console.log(`Seeding complete.`);
+  })
+  .get("/result", async () => {
+    const result = await db.select().from(schema.movies);
+    return result;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
