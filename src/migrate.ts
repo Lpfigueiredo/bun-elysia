0227255d10e1d2a8df117660bfcaff2d22034990
Leonardo Figueiredo <leonardo.figueiredo@ipflagship.com.br>
2024-01-19 15:11:15 -0300

    format

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,7 +1,7 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 
 const sqlite = new Database("sqlite.db");
 const db = drizzle(sqlite);
